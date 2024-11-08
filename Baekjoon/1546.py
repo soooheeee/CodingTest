de001648e1f362c8dd8031087bd0d309e66ee250
yn_nub <129060841+soooheeee@users.noreply.github.com>
2024-11-08 20:52:23 +0900

    [BOJ] 1546번: 평균

---
+++ Baekjoon/1546.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+li=[]
+sum=0
+n=int(input())
+
+li=list(map(int,input().split()))
+max = li[0] 
+
+for i in range (n):
+    if max <=li[i]:
+        max=li[i]
+for j in range (n):
+    change=li[j]/max*100
+    sum+=change
+avg=sum/n
+# print(li)
+# print(max)
+print(avg)
+
+
+# 두 번째 코드 
+# 내장 함수 사용
+N=int(input())
+L=list(map(int,input().split()))
+print(sum(L)/max(L)*100/N)
