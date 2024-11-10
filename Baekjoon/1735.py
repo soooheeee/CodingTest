59ebec141cd7e62fbd6209b5bffdd6d0ee01d1d5
yn_nub <129060841+soooheeee@users.noreply.github.com>
2024-11-10 23:11:13 +0900

    [BOJ] 1735번: 분수 합

---
+++ Baekjoon/1735.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+import math
+
+a,b =map(int,input().split())
+c,d=map(int,input().split())
+
+max=math.gcd(b*d,(a*d)+(b*c))
+bunmo=b*d//max
+bunza=((a*d)+(b*c))//max
+print(bunza,bunmo)
